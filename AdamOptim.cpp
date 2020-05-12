5fe3a5d7c033a6b6ccd338945b678265f7036175
SkymePC <skyme@localhost.localdomain>
2020-05-12 18:05:55 +0300

    optimizer tested

--- AdamOptim.cpp
+++ AdamOptim.cpp
@@ -1,25 +1,34 @@
 #include "NeuralNet.hpp"
-#include <Matrix.hpp>
+#include "Matrix.hpp"
+#include <math.h>
 
 template <class T>
-nn::AdamOptim<T>::AdamOptim (T beta_1 = 0.9, T beta_2 = 0.999, T epsilon = 1e-08) {
+nn::AdamOptim<T>::AdamOptim (T beta_1, T beta_2, T epsilon) {
     this->beta_1 = beta_1;
     this->beta_2 = beta_2;
     this->epsilon = epsilon;
+    this->momentum = 0;
     this->velocity = 0;
     this->t = 1;
 }
 
 template <class T>
 Matrix nn::AdamOptim<T>::update (Matrix w, Matrix d_w, T learning_rate){
+    // Momentum update
+    Matrix beta_momentum = this->momentum * this->beta_1;
+    this->momentum = d_w * (1 - this->beta_1) + beta_momentum;
+    // Velocity update
+    Matrix beta_velocity = this->velocity * this->beta_2;
+    this->velocity = ((d_w ^ 2.0) * (1 - this->beta_2)) + beta_velocity;
 
-    this->momentum = this->beta_1 * this->momentum + (1 - this->beta_2) * d_w;
-    this->velocity = this->beta_2 * this->velocity + (1 - this->beta_2) * d_w ^ 2;
-
-    Matrix m_hat;
-    Matrix v_hat;
+    Matrix m_hat = this->momentum / (1 - std::pow(this->beta_1, this->t));
+    Matrix v_hat = this->velocity / (1 - std::pow(this->beta_2, this->t));
 
     this->t++;
 
-    return w - learning_rate * m_hat / (v_hat.sqrt() + this->epsilon);
+    m_hat = (m_hat * learning_rate) / (v_hat.sqrt() + this->epsilon);
+
+    return w - m_hat;
 }
+
+template class nn::AdamOptim<double>;
