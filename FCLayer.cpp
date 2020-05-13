0a304e4a5b8836f3e81c63e985279bbd70fb6640
SkymePC <skyme@localhost.localdomain>
2020-05-13 11:13:00 +0300

    minor changes

--- FCLayer.cpp
+++ FCLayer.cpp
@@ -3,16 +3,16 @@
 using nn::Parameter;
 
 nn::FCLayer::FCLayer (int n_input, int n_output) {
-    this->W = Parameter(Matrix(n_input, n_output).fill_rand());
+    this->W = Parameter(Matrix(n_input, n_output).fill_rand() * 0.001);
     this->B = Parameter(Matrix(1, n_output).fill_rand() * 0.001);
 }
 
-Matrix nn::FCLayer::forward (Matrix X) {
+Matrix nn::FCLayer::forward (Matrix &X) {
     this->X = X;
     return this->X.dot(this->W.value) + this->B.value;
 }
 
-Matrix nn::FCLayer::backward (Matrix d_out) {
+Matrix nn::FCLayer::backward (Matrix &d_out) {
     // W gradient computing
     Matrix w_grad = this->X.T().dot(d_out);
     this->W.grad = this->W.grad + w_grad;
