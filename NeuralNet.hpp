a17581d42acf1be8e6b8bf0c8e0cccfea25be091
SkymePC <skyme@localhost.localdomain>
2020-05-13 08:24:38 +0300

    Softmax layer; Parameter is fully public now; comments added

--- NeuralNet.hpp
+++ NeuralNet.hpp
@@ -2,7 +2,19 @@
 #include <vector>
 #include "Matrix.hpp"
 
-
+/**********************************************************
+ * nn - Neural Network namespace, contains all classes to create a simple perceptrone model.
+ * --------------------------------------------------------
+ * classes:
+ *   Model - NN model, consist of some layers
+ *   Parameter - Matrix based parameter for layers
+ *   FCLayer - fully connected layer, parameters: w, b
+ *   ReLULayer - ReLU layer, parameters: no params
+ *   SoftmaxLayer - softmax, cross-entropy and l2 static functions
+ *   AdamOptim - adam optimizer, parameters: beta1, beta2, epsilon
+ * --------------------------------------------------------
+ * Last changes 13 may 2020 by Skyme Factor.
+ **********************************************************/
 namespace nn {
 
     class Model {
@@ -16,11 +28,11 @@ namespace nn {
     };
 
     class Parameter {
-    private:
+    public:
         Matrix value;
         Matrix grad;
-    public:
-        Parameter (Matrix value) {
+        Parameter () {};
+        explicit Parameter (Matrix value) {
             this->value = value;
             this->grad = Matrix(value.shape());
         }
@@ -28,12 +40,13 @@ namespace nn {
 
     class FCLayer {
     private:
-        Matrix W, B, X;
+        Parameter W, B;
+        Matrix X;
     public:
-        FCLayer (int n_input, int n_output);
+        explicit FCLayer (int n_input, int n_output);
         Matrix forward (Matrix X);
         Matrix backward (Matrix d_out);
-        std::pair<Matrix, Matrix> get_params ();
+        std::pair<Parameter, Parameter> get_params ();
     };
 
     class ReLULayer {
@@ -45,17 +58,38 @@ namespace nn {
         Matrix backward (Matrix d_out);
     };
 
+    class SoftmaxLayer {
+    public:
+        static std::pair<double, Matrix> l2_reg (Matrix &, const double &);
+        static Matrix softmax (Matrix &);
+        static double ce_loss (Matrix &, Matrix &);
+        static std::pair<double, Matrix> softmax_with_ce_loss (Matrix &, Matrix &);
+    };
+
     template <class T>
     class AdamOptim {
     private:
         T beta_1;
         T beta_2;
         T epsilon;
-        T momentum;
-        T velocity;
-        int t;
+        Matrix momentum;
+        Matrix velocity;
+        T t;
     public:
-        explicit AdamOptim<T> (T beta_1, T beta_2, T epsilon);
+        /*
+        * Explicit constructor of class AdamOptim
+        * Parameters:
+        *   <T> beta1 - default 0.9
+        *   <T> beta2 - default 0.999
+        *   <T> epsilon - default 1e-8
+        */
+        explicit AdamOptim<T> (T beta_1 = 0.9, T beta_2 = 0.999, T epsilon = 1e-08);
+        /*
+        * Parameters:
+        *   Matrix w - model parameter
+        *   Matrix d_w - gradient for parameter
+        *   <T> learning_rate - learning rate for the model
+        */
         Matrix update (Matrix w, Matrix d_w, T learning_rate);
     };
 }
