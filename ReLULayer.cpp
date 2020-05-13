a47b2a534db36dd8f2b2c4c07233468808b916e8
SkymePC <skyme@localhost.localdomain>
2020-05-13 11:13:54 +0300

    minor changes

--- ReLULayer.cpp
+++ ReLULayer.cpp
@@ -1,12 +1,12 @@
 #include "NeuralNet.hpp"
 
 
-Matrix nn::ReLULayer::forward (Matrix X) {
-    this->result = (X > 0);
-    return this->result * X;
+Matrix nn::ReLULayer::forward (Matrix &X) {
+    this->X = (X > 0);
+    return this->X * X;
 }
 
-Matrix nn::ReLULayer::backward (Matrix d_out) {
-    Matrix d_result = d_out * this->result;
+Matrix nn::ReLULayer::backward (Matrix &d_out) {
+    Matrix d_result = d_out * this->X;
     return d_result;
 }
